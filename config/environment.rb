510793e1ba0c133550ffb83810119c9ba697fec3
keykeyair <keykeyair@gmail.com>
2011-07-23 21:14:25 +0800

    Initial2.

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+TestUp2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestUp2::Application.config.secret_token = 'c1cf9c747d7400f9568f35a3e698fd376faf4e66eeb9d67b06d7b7ca1aa062849e61647f875fb25f633f948d8af74484c9cd7e2615443e1ffdd2076affb5c34c'
